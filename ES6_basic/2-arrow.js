0a53acecf8a946fb72f0ee9b268ed27887357430
leyvsi <sylviecosta78500@gmail.com>
2026-04-28 15:09:07 +0200

    add task 2

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a list of neighborhoods in San Francisco.
+ * Uses ES6 arrow function to maintain 'this' context.
+ */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
