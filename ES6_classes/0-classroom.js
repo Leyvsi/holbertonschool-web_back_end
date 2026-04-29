7ce42e6c86d13dcecc7cc9ffed0681924a1dae29
leyvsi <sylviecosta78500@gmail.com>
2026-04-29 15:45:15 +0200

    add task 0

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+export default class ClassRoom {
+  /**
+   * Represents a classroom.
+   * @param {Number} maxStudentsSize - The maximum number of students allowed.
+   */
+  constructor(maxStudentsSize) {
+    // Assign the maximum student size to the private attribute _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
